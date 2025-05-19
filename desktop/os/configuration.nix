135cfef3ea0b3c76aea1062381fe15a8e94cec55
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-05-19 16:32:01 -0300

    add zerotier

--- desktop/os/configuration.nix
+++ desktop/os/configuration.nix
@@ -4,6 +4,7 @@
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../common.nix
+    ./modules/zerotier/zerotier.nix
   ];
 
   nix = {
---
+++ desktop/os/modules/zerotier/zerotier.nix
@@ -0,0 +1,9 @@
+{
+
+    services.zerotierone = {
+        enable = true;
+        joinNetworks = [
+            "565799d8f682f681"
+        ];
+};
+}
